7f0ce86d5e9f56b3464c433e74a0356d6a480fc6
pancham01 <pancham9557@gmail.com>
2025-11-05 12:18:06 +0530

    update

--- src/main/java/com/learning/hbn/Main.java
+++ src/main/java/com/learning/hbn/Main.java
@@ -10,24 +10,31 @@ import com.learning.hbn.entity.Employee;
 
 public class Main {
 	public static void main(String[] args) {
-		Employee emp = new Employee("Ashish","Male",45454);
-		
-		Address add = new Address("Noida","UP");
-		
-		
-		emp.setAddress(add);
-		
+//		Employee emp = new Employee("Vinay", "Male", 45454);
+//		Address add = new Address("GZB", "UP");
+//		emp.setAddress(add);
+//		add.setEmployee(emp);
+
 		SessionFactory sessionFactory = HibernateConfiguration.getSessionFactory();
 		Session session = sessionFactory.openSession();
 		Transaction tx = session.beginTransaction();
-		
-//		session.persist(emp);
-//		session.persist(add);
-		
-		System.out.println("Employee : "+session.get(Employee.class, 1));
-		System.out.println("Address : "+session.get(Address.class, 1));
-		
+
+		getEmpByIdWithAdd(session);
+		getAddByIdWithEmp(session);
+
 		tx.commit();
 	}
 
+	public static void getEmpByIdWithAdd(Session session) {
+		Employee employee = session.get(Employee.class, 1);
+		System.out.println("employee : " + employee);
+		System.out.println("employee.getAddress : " + employee.getAddress());
+
+	}
+
+	public static void getAddByIdWithEmp(Session session) {
+		Address address = session.get(Address.class, 2);
+		System.out.println("address : " + address);
+		System.out.println("address.getEmployee : " + address.getEmployee());
+	}
 }
--- src/main/java/com/learning/hbn/entity/Employee.java
+++ src/main/java/com/learning/hbn/entity/Employee.java
@@ -71,7 +71,6 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + ", address="
-				+ address + "]";
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary +  "]";
 	}
 }
